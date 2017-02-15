37a8a9661cb06f9dae6789127d670f8c9881bc1b
Isaac Cohen <isaaclandoncohen@gmail.com>
2017-02-14 19:07:39 -0800

    bonez werk

---
+++ Assets/BoneLearn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneLearn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		SkinnedMeshRenderer skinnedMesh = GetComponent<SkinnedMeshRenderer>();
+		Debug.Log( skinnedMesh.bones.Length );
+		for( int i = 0; i < skinnedMesh.bones.Length; i++){
+			Debug.Log( skinnedMesh.bones[i]);
+		}
+
+
+		//Debug.Log( skinnedMesh.bones[0].children );
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
